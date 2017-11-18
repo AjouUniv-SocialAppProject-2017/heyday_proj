6e9e1c43150598b8f3da8730c0496ab727717030
ho0kim <hoyoung7413@gmail.com>
2017-11-18 18:31:34 +0900

    edit login activity finish()

--- app/src/main/java/com/example/choikim/hayday_proj/LoginActivity.java
+++ app/src/main/java/com/example/choikim/hayday_proj/LoginActivity.java
@@ -64,6 +64,7 @@ public class LoginActivity extends Activity {
                 if (user != null) {
                     Intent intent = new Intent(LoginActivity.this,MainActivity.class);
                     startActivity(intent);
+                    finish();
 
                 } else {
 
