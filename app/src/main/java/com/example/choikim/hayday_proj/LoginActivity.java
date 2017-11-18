1523d85c92128ce987bb3198262559a98ffd3eab
ho0kim <hoyoung7413@gmail.com>
2017-11-18 16:56:28 +0900

    facebook login bundle setting menifest, layout setting

--- app/src/main/java/com/example/choikim/hayday_proj/LoginActivity.java
+++ app/src/main/java/com/example/choikim/hayday_proj/LoginActivity.java
@@ -1,8 +1,16 @@
 package com.example.choikim.hayday_proj;
 
+import android.app.Activity;
+import android.os.Bundle;
+
 /**
  * Created by khy12 on 2017-11-18.
  */
 
-public class LoginActivity {
+public class LoginActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.acrivity_login);
+    }
 }
