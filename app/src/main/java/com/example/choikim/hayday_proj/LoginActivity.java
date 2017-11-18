128a52a145a245614b92733853c13d509dfcd382
ho0kim <hoyoung7413@gmail.com>
2017-11-18 18:16:49 +0900

    finish login & logout

--- app/src/main/java/com/example/choikim/hayday_proj/LoginActivity.java
+++ app/src/main/java/com/example/choikim/hayday_proj/LoginActivity.java
@@ -30,6 +30,7 @@ public class LoginActivity extends Activity {
 
     private CallbackManager mCallbackManager;
     private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -55,6 +56,20 @@ public class LoginActivity extends Activity {
             public void onError(FacebookException error) {
                 }
         });
+
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+                    Intent intent = new Intent(LoginActivity.this,MainActivity.class);
+                    startActivity(intent);
+
+                } else {
+
+                }
+            }
+        };
     }
 
     private void handleFacebookAccessToken(AccessToken token) {
@@ -66,13 +81,47 @@ public class LoginActivity extends Activity {
                     public void onComplete(@NonNull com.google.android.gms.tasks.Task<AuthResult> task) {
                         if (task.isSuccessful()) {
                             // Sign in success, update UI with the signed-in user's information
-                            Toast.makeText(LoginActivity.this, "로그인 실패", Toast.LENGTH_SHORT).show();
+                            Toast.makeText(LoginActivity.this, "로그인 성공", Toast.LENGTH_SHORT).show();
                             FirebaseUser user = mAuth.getCurrentUser();
 
                         } else {
                             // If sign in fails, display a message to the user.
-                            Toast.makeText(LoginActivity.this, "로그인 성공",
+                            Toast.makeText(LoginActivity.this, "로그인 실패",
                                     Toast.LENGTH_SHORT).show();
+
+                        }
+                    }
+                });
+    }
+
+    private void createUser(final String email, final String password){
+        mAuth.createUserWithEmailAndPassword(email,password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull com.google.android.gms.tasks.Task<AuthResult> task) {
+                        if(task.isSuccessful()){
+                            loginUser(email,password);
+                        }else{
+                            Toast.makeText(LoginActivity.this, "회원가입 성공", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+
+    }
+    private void loginUser(String email, String password){
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull com.google.android.gms.tasks.Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+
+                            FirebaseUser user = mAuth.getCurrentUser();
+
+                        } else {
+                            // If sign in fails, display a message to the user.
+
+                            Toast.makeText(LoginActivity.this, "이메일 로그인 완료", Toast.LENGTH_SHORT).show();
 
                         }
                     }
@@ -85,5 +134,21 @@ public class LoginActivity extends Activity {
 
         // Pass the activity result back to the Facebook SDK
         mCallbackManager.onActivityResult(requestCode, resultCode, data);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        mAuth.addAuthStateListener(mAuthListener);
+    }
+
+    @Override
+    public void onStop(){
+        super.onStop();
+        if(mAuthListener!=null){
+            mAuth.removeAuthStateListener(mAuthListener);
+        }
+
     }
 }
--- app/src/main/java/com/example/choikim/hayday_proj/MainActivity.java
+++ app/src/main/java/com/example/choikim/hayday_proj/MainActivity.java
@@ -1,13 +1,43 @@
 package com.example.choikim.hayday_proj;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.facebook.login.LoginManager;
+import com.google.firebase.auth.FirebaseAuth;
+
+import java.util.EventListener;
 
 public class MainActivity extends AppCompatActivity {
-
+    private TextView userName;
+    private Button btnLogout;
+    private FirebaseAuth auth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        auth = FirebaseAuth.getInstance();
+
+        userName=(TextView)findViewById(R.id.user_name);
+        btnLogout=(Button)findViewById(R.id.btn_logout);
+
+        userName.setText(auth.getCurrentUser().getDisplayName());
+        btnLogout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                auth.signOut();
+                LoginManager.getInstance().logOut();
+                finish();
+                Intent intent =new Intent(MainActivity.this,LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
     }
 }
