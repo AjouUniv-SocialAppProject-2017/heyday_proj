0faf0dc082972ed4cf879968c4dbec1ca7b017b6
ho0kim <hoyoung7413@gmail.com>
2017-12-02 17:17:26 +0900

    boardfragment recyclerview finish

--- app/src/main/java/com/example/choikim/hayday_proj/fragment/BoardFragment.java
+++ app/src/main/java/com/example/choikim/hayday_proj/fragment/BoardFragment.java
@@ -1,9 +1,10 @@
 package com.example.choikim.hayday_proj.fragment;
 
-import android.app.Fragment;
+import android.support.v4.app.Fragment;;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -50,6 +51,9 @@ public class BoardFragment extends Fragment{
                 startActivity(intent);
             }
         });
+        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+        recyclerView.setAdapter(new RecyclerViewAdapter());
+
 
         return view;
     }
