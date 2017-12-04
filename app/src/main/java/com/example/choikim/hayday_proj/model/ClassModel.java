1cda08de0dc490e4b39e9920553b85b6eee60638
Jae Young <jamting122@ajou.ac.kr>
2017-12-04 12:42:12 +0900

    add classmodel

---
+++ app/src/main/java/com/example/choikim/hayday_proj/model/ClassModel.java
@@ -0,0 +1,19 @@
+package com.example.choikim.hayday_proj.model;
+
+/**
+ * Created by jamti on 2017-12-02.
+ */
+
+public class ClassModel {
+
+    public String class_image;
+    public String class_name;
+    public String class_index;
+    public String class_content;
+    public String uid;
+    public String userId;
+    public String user_name;
+
+
+
+}
