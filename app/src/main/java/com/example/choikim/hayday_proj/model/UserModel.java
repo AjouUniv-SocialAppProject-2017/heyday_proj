54860df18f05af0286993b5611abb1bd4f9e8c6b
ho0kim <hoyoung7413@gmail.com>
2017-12-15 02:01:09 +0900

    usermodel edited

--- app/src/main/java/com/example/choikim/hayday_proj/model/UserModel.java
+++ app/src/main/java/com/example/choikim/hayday_proj/model/UserModel.java
@@ -1,5 +1,8 @@
 package com.example.choikim.hayday_proj.model;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Created by khy12 on 2017-11-21.
  */
@@ -9,7 +12,8 @@ public class UserModel {
     public String name;
     public String profileImagePath;
     public String email;
+    public String backgroundImagePath;
 
-
+    public Map<String, Boolean> preference= new HashMap<>();
 
 }
