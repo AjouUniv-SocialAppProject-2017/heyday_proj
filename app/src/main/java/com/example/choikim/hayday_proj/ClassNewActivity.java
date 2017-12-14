cf9c05ee97f37a7e6dec79e5bfc0308d22d2e5f5
Jae Young <jamting122@ajou.ac.kr>
2017-12-14 17:25:28 +0900

    add ClassBestActivity and ClassNewActivity

--- app/src/main/java/com/example/choikim/hayday_proj/ClassNewActivity.java
+++ app/src/main/java/com/example/choikim/hayday_proj/ClassNewActivity.java
@@ -1,8 +1,8 @@
 package com.example.choikim.hayday_proj;
 
 import android.content.Intent;
-import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.text.Editable;
@@ -26,13 +26,7 @@ import com.google.firebase.database.ValueEventListener;
 import java.util.ArrayList;
 import java.util.List;
 
-import static java.sql.Types.NULL;
-
-/**
- * Created by jamti on 2017-11-30.
- */
-
-public class ClassActivity extends AppCompatActivity{
+public class ClassNewActivity extends AppCompatActivity {
 
     private RecyclerView recyclerView;
 
@@ -45,10 +39,7 @@ public class ClassActivity extends AppCompatActivity{
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_class);
-
-        Intent intent = getIntent();
-        final String class_index = intent.getExtras().getString("value");
+        setContentView(R.layout.activity_class_new);
 
         auth = FirebaseAuth.getInstance();
         database = FirebaseDatabase.getInstance();
@@ -56,6 +47,7 @@ public class ClassActivity extends AppCompatActivity{
         recyclerView  = (RecyclerView)findViewById(R.id.recycle_view);
 
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
         final ClassRecyclerViewAdapter classRecyclerViewAdapter = new ClassRecyclerViewAdapter();
         recyclerView.setAdapter(classRecyclerViewAdapter);
 
@@ -65,16 +57,8 @@ public class ClassActivity extends AppCompatActivity{
                 classdata.clear();
                 for(DataSnapshot snapshot : dataSnapshot.getChildren()){
                     ClassModel classmodel = snapshot.getValue(ClassModel.class);
-                    if(class_index.equals("8")) {
-                        classdata.add(classmodel);
-                        searchdata.add(classmodel);
-                    }
-                    else if(class_index.equals( classmodel.class_index)==true){
-                        classdata.add(classmodel);
-                        searchdata.add(classmodel);
-                    }
-
-
+                    classdata.add(classmodel);
+                    searchdata.add(classmodel);
                 }
                 classRecyclerViewAdapter.notifyDataSetChanged();
 
@@ -113,7 +97,6 @@ public class ClassActivity extends AppCompatActivity{
 
     }
 
-
     class ClassRecyclerViewAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
 
 
@@ -143,9 +126,9 @@ public class ClassActivity extends AppCompatActivity{
         @Override
         public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
 
-            ((CustomViewHolder)holder).class_teacher.setText(classdata.get(position).user_name);
-            ((CustomViewHolder)holder).class_name.setText(classdata.get(position).class_name);
-            Glide.with(holder.itemView.getContext()).load(classdata.get(position).class_image).into(((CustomViewHolder)holder).class_image);
+            ((ClassNewActivity.ClassRecyclerViewAdapter.CustomViewHolder)holder).class_teacher.setText(classdata.get(position).user_name);
+            ((ClassNewActivity.ClassRecyclerViewAdapter.CustomViewHolder)holder).class_name.setText(classdata.get(position).class_name);
+            Glide.with(holder.itemView.getContext()).load(classdata.get(position).class_image).into(((ClassNewActivity.ClassRecyclerViewAdapter.CustomViewHolder)holder).class_image);
         }
 
         @Override
@@ -158,12 +141,10 @@ public class ClassActivity extends AppCompatActivity{
         @Override
         public RecyclerView.ViewHolder onCreateViewHolder(final ViewGroup parent, int viewType) {
             View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_class,parent,false);
-            return new CustomViewHolder(view);
+            return new ClassNewActivity.ClassRecyclerViewAdapter.CustomViewHolder(view);
         }
 
 
 
     }
-
-
 }
